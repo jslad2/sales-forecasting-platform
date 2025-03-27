37d1ee9c845602034239c8fce883200768bedfaa
jslad2 <jslad13@gmail.com>
2025-03-27 07:38:28 -0500

    z

--- forecasting_tool.py
+++ forecasting_tool.py
@@ -607,7 +607,7 @@ def train_automl_model(train, test, forecast_period, last_historical_value, y_or
 
         # Create forecast DataFrame
         forecast_df = pd.DataFrame({
-            "ds": pd.date_range(start=train["ds"].iloc[-1] + pd.DateOffset(months=1), periods=forecast_period, freq="M"),
+            "ds": pd.date_range(start=train["ds"].iloc[-1] + pd.DateOffset(months=1), periods=forecast_period, freq="MS"),
             "yhat": automl_forecast,
             "yhat_lower": automl_forecast * 0.9,
             "yhat_upper": automl_forecast * 1.1
